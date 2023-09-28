b0b95630c905d314bc1ea1dfcdb0acf294ad860c
jcblanc2 <jclaytonblanc@gmail.com>
2023-09-28 14:35:09 -0400

    Add login route

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const authRouter = require('./routes/auth');
 const mongoose = require('mongoose');
 require('dotenv/config');
 
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => console.log("Connected to DB!"));
+
 // Middleware
 app.use(express.json());
 app.use(cors());
+// Routes Middleware
 app.use('/api/user', authRouter);
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello World! -- Home');
-});
-
-// Connect to DB
-// mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => console.log("Connected to DB!"));
 
 // Start Server
 app.listen(3000, () => {console.log(`Server is running! http://localhost:${process.env.PORT}`)});
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,58 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const User = require("../models/User");
+const {registerValidation, loginValidation} = require("../utils/validation");
+const bcrypt = require("bcryptjs");
 
-// Routes
-router.post('/register', (req, res) => {
-    res.send('register');
+// Route register
+router.post("/register", async (req, res) => {
+    // validate the data
+    const {error} = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Check if the user already exists
+    const emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) return res.status(400).send({message: "Email already exists"});
+
+    // Hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    // Crete a new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+    });
+
+    try{
+        const userSaved = await user.save();
+        res.status(201).send({userId: user._id});
+    }
+    catch(err){
+        res.status(400).send({message:err});
+    }
 });
 
-router.post('/login', (req, res) => {
-    res.send('login');
+// Route login
+router.post("/login", async (req, res) => {
+    // validate the data
+    const {error} = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Check if the user already exists
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send({message: "Email or Passord is wrong!"});
+
+    // Check if the password is correct
+    const hashedPassword = bcrypt.compareSync(req.body.password, user.password);
+    if (!hashedPassword) return res.status(400).send({message: "Email or Passord is wrong!"});
+
+    try{
+        res.status(200).send({userId: user._id});
+    }
+    catch(err){
+        res.status(400).send({message:err});
+    }
 });
 
 module.exports = router;
